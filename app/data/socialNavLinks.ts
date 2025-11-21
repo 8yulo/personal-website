ad98f83853696fe132bc55ad0346069da05501ec
8yulo <47400802+8yulo@users.noreply.github.com>
2025-11-20 21:48:31 -0800

    social links footer

---
+++ app/data/socialNavLinks.ts
@@ -0,0 +1,7 @@
+const socialNavLinks = [
+  { ref: "https://www.linkedin.com/in/yulo/", src: "/images/linkedin-icon.svg", title: "Linkedin"  },
+  { ref: "https://x.com/realmathgeek", src: "/images/x-icon.svg", title: "X"  },
+  { ref: "https://github.com/8yulo", src: "/images/github-icon.svg", title: "GitHub" },
+];
+
+export default socialNavLinks;

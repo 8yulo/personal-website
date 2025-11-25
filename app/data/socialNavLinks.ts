7505f2f2a1c67968ff8e70077ab691a44b51ce7b
8yulo <47400802+8yulo@users.noreply.github.com>
2025-11-24 21:46:42 -0800

    final phase

--- app/data/socialNavLinks.ts
+++ app/data/socialNavLinks.ts
@@ -1,7 +1,7 @@
 const socialNavLinks = [
-  { ref: "https://www.linkedin.com/in/yulo/", src: "/images/linkedin-icon.svg", title: "Linkedin"  },
-  { ref: "https://x.com/realmathgeek", src: "/images/x-icon.svg", title: "X"  },
-  { ref: "https://github.com/8yulo", src: "/images/github-icon.svg", title: "GitHub" },
+  { ref: "https://www.linkedin.com/in/yulo/", src: "public/images/linkedin-icon.svg", title: "Linkedin"  },
+  { ref: "https://x.com/realmathgeek", src: "public/images/x-icon.svg", title: "X"  },
+  { ref: "https://github.com/8yulo", src: "public/images/github-icon.svg", title: "GitHub" },
 ];
 
 export default socialNavLinks;
